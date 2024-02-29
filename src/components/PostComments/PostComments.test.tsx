20a90381f9e6d5873ef35ee62c258867d0b3bf81
TamaradeMoura <tamara_moura_13@hotmail.com>
2024-02-29 14:55:48 -0300

    commit dos testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,26 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    })
+
+    test('Teste para renderizar os comentário', () => {
+        render(<PostComment/>)
+        fireEvent.change(screen.getByTestId('comentarios'), {
+            target: {
+                value: 'Parabéns'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentario'))
+        
+        fireEvent.change(screen.getByTestId('comentarios'), {
+            target: {
+                value: 'show'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentario'))
+        expect(screen.getAllByTestId('campo-comentario')).toHaveLength(2)
+    })
 });
